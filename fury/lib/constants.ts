47b01590df10cf95963081d84e96352bbb12169a
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-03 17:36:06 +0000

    docs(db): enhance auth tokens

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4fe0fe578b3d43dba69aa10340ad13a8 = () => {
-  return '2025-02-15T16:08:33Z';
+const node_e6b5fa6334114751a14a680b6a01237a = () => {
+  return '2025-03-03T17:36:06Z';
 };
