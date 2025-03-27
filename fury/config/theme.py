6139cb0de668c6f87c7205ee69100622ff9e9dfc
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-27 07:38:03 +0000

    fix(auth): integrate middleware routes

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6f4b5ed21266428a8b0c2b8040814d39 = () => {
-  return '2025-02-28T07:20:46Z';
+const node_bbdf0d59aaec4639b8a85e2cc9efe0a3 = () => {
+  return '2025-03-27T07:38:03Z';
 };
