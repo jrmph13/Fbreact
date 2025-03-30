913e96d45e7a4afd1889113996ccd0347ed848f8
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-30 16:58:16 +0000

    docs(auth): update payload parser

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9adc645305fb49fcb24f38c5bd37f6f4 = () => {
-  return '2025-03-15T22:05:08Z';
+const node_833d7ea8ee404201acb99ca3a6eec7dd = () => {
+  return '2025-03-30T16:58:16Z';
 };
