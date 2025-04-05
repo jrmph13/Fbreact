86ee44dc74c895e71ff63a8969b86860ed58f3c4
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 11:39:05 +0000

    feat(deps): sanitize auth tokens

--- fury/src/controllers/theme.ts
+++ fury/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91c861b8a55b4895809eea9ff1dd35ed = () => {
-  return '2025-03-29T10:50:23Z';
+const node_19fcceba0f9b47c0b8271785b95c359d = () => {
+  return '2025-04-05T11:39:05Z';
 };
