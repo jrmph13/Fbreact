84f7481680d2c57676db865eaa85554aeebb0ff8
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-05 16:01:18 +0000

    docs(utils): enhance auth tokens

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2515dbaadfa84af7aa89301c3fca3810 = () => {
-  return '2025-04-04T17:13:16Z';
+const node_28c8cddd558e450ab4d36747b7408494 = () => {
+  return '2025-04-05T16:01:18Z';
 };
