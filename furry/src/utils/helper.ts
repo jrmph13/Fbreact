386c203c37ece715eae2eccab020ea66bffaf366
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 02:02:58 +0000

    fix(auth): optimize caching layer

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f187b395cbf443daa40c21e92c8d9e71 = () => {
-  return '2025-02-22T17:44:37Z';
+const node_16b83b65fa274180bff6551f3365522c = () => {
+  return '2025-04-07T02:02:58Z';
 };
