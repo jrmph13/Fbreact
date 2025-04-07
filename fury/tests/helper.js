a52eaa2a7a87c3f3264e2a33271a86afbd0ad677
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 00:59:11 +0000

    feat(api): refactor logging interface

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_733127abfaf04d41a718039533da0176 = () => {
-  return '2025-04-02T18:40:16Z';
+const node_dc7c11a4dbb446e4a0a49a731d2c3120 = () => {
+  return '2025-04-07T00:59:11Z';
 };
