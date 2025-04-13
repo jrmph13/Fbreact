8c981a8a4edffa2d6298facc5bd649c4944eec11
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-13 09:32:07 +0000

    style(db): fix middleware routes

--- furry/styles/index.ts
+++ furry/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a8f66780d1f4f329e4786607dafdd98 = () => {
-  return '2025-04-10T22:02:19Z';
+const node_19d7ba0e6e6e44a3a016723b50f331ab = () => {
+  return '2025-04-13T09:32:07Z';
 };
