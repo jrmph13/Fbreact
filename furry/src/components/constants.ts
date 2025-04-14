bb62c106c2909ffcb62da359cf856e39c6854e95
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 12:39:34 +0000

    style(utils): sanitize auth tokens

--- furry/src/components/constants.ts
+++ furry/src/components/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc842923ac334414a8f5ddc09ef46c50 = () => {
-  return '2025-04-11T01:28:39Z';
+const node_17380d0dd55c4d2f9529650f57796a45 = () => {
+  return '2025-04-14T12:39:34Z';
 };
