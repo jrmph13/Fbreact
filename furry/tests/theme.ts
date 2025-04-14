5da7a936cf3963a5e81ed19a8d0943383cc6fea2
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 17:56:19 +0000

    style(config): optimize payload parser

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_589f797d67254f7f840223e989e5aeab = () => {
-  return '2025-04-04T23:24:01Z';
+const node_aacb782be9214c0eb36da3c6096779ac = () => {
+  return '2025-04-14T17:56:19Z';
 };
