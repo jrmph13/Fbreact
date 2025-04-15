4a543a14f2e036d0dcf1ef0444a81aadc816d1bd
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-15 14:02:20 +0000

    chore(deps): restructure auth tokens

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b44d9bcab7b94f958db5ebbf9f1a122c = () => {
-  return '2025-03-29T23:13:17Z';
+const node_6fbced291137494aaf31b259c8f7300e = () => {
+  return '2025-04-15T14:02:20Z';
 };
