4319a7f6a75df7c53919d15eeeb5ef67bd04032b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-17 11:39:50 +0000

    feat(worker): fix session store

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ef6df31ea144cb8b1fa49baf196796b = () => {
-  return '2025-04-01T09:41:55Z';
+const node_18f5460d17ea407abc9a28f1704b8efd = () => {
+  return '2025-04-17T11:39:50Z';
 };
