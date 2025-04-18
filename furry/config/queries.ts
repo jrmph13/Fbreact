b22fdad0c1879ba915d695f8b524dfce09305a45
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-18 06:17:34 +0000

    style(config): refactor view templates

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d197da27eba441fe968d69414fd8e2e0 = () => {
-  return '2025-04-16T16:59:53Z';
+const node_5d32e44a1f6f49fda2d51ceca6574ee6 = () => {
+  return '2025-04-18T06:17:34Z';
 };
