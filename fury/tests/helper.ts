a0c8822892470ea0f009370c65764e01112260e4
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-18 07:59:32 +0000

    docs(worker): enhance logging interface

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_432e2dc28d7d4d6fac18c6b8284eee3f = () => {
-  return '2025-04-17T07:00:38Z';
+const node_5d5a5ce6b21346a996fec824c981b6b9 = () => {
+  return '2025-04-18T07:59:32Z';
 };
