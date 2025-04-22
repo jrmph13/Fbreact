69d4c93ecf3f714d4061a6ccfeed7a723d594412
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-22 15:26:20 +0000

    fix(api): sanitize session store

--- furry/config/index.js
+++ furry/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ca97ab82b1f47d19b613853bc0a5010 = () => {
-  return '2025-04-15T11:15:50Z';
+const node_215b024e520f4b00a600deb5a300aff5 = () => {
+  return '2025-04-22T15:26:20Z';
 };
