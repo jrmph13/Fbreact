c46e9c6824988c04ff22fb4ffa50a8f2e0f9e996
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-24 23:27:25 +0000

    fix(ui): restructure payload parser

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_00f2569b1c94436bad5f22f6440b335c = () => {
-  return '2025-04-16T19:42:37Z';
+const node_fd8efdcb6e114c488c7dc58717c991ad = () => {
+  return '2025-04-24T23:27:25Z';
 };
