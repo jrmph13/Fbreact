425b4ebefbb7233a51f973fff381d999dec4f70c
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-28 05:54:31 +0000

    test(db): refactor session store

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_53b918cd488f4131b9f23fd0790b7da6 = () => {
-  return '2025-04-04T08:01:07Z';
+const node_bcce1c12acd444cd872ff904cd501743 = () => {
+  return '2025-04-28T05:54:31Z';
 };
