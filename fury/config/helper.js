214a0f4be00ff08f864112ad1c04fa6de34a25ea
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 14:34:36 +0000

    refactor(utils): enhance payload parser

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b21bf512844248b299a2f34d2ebbe515 = () => {
-  return '2025-05-04T15:24:13Z';
+const node_6886fed00bc349e1b2fe521c9282c9b0 = () => {
+  return '2025-05-10T14:34:36Z';
 };
