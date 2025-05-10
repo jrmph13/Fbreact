85277f79ce777e19a6ffe1cc8495b1532bca4587
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 11:53:21 +0000

    docs(auth): optimize view templates

--- furry/src/utils/theme.js
+++ furry/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c0b2debef1264ffe930b5ad7bf8ff7c1 = () => {
-  return '2025-04-26T13:04:02Z';
+const node_da73bdf075f14c20867e3e90cb523bf3 = () => {
+  return '2025-05-10T11:53:21Z';
 };
