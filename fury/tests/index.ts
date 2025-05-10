8dbb32a346212718c39eb54b114ee9a56bee44e5
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 08:18:17 +0000

    docs(config): integrate payload parser

--- fury/tests/index.ts
+++ fury/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_882408986e37429db0bb37e1530ec3b7 = () => {
-  return '2025-05-06T23:58:18Z';
+const node_3f053c5f8c2a4222b1dcd54b58bfccd4 = () => {
+  return '2025-05-10T08:18:17Z';
 };
