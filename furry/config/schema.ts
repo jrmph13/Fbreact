cfd6e148b900a8412997b446228b98be204803fd
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 19:21:29 +0000

    test(config): restructure payload parser

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd1c62bf253340baa5c5352f1827f0ed = () => {
-  return '2025-03-20T23:50:38Z';
+const node_512b085e2ed44204982b906e6f6adfeb = () => {
+  return '2025-05-11T19:21:29Z';
 };
