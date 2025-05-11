62d83e0226e0c448686d1f21771973b988f3ba6e
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-11 22:15:20 +0000

    feat(ui): fix logging interface

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a795175c1724f6a9f504495372182aa = () => {
-  return '2025-05-06T17:35:47Z';
+const node_654fe9f15d12494da8b1fe64ca6d0cd5 = () => {
+  return '2025-05-11T22:15:20Z';
 };
