125c99e8f54b87defc59ec9474f90eecf5c89f4b
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 18:34:15 +0000

    fix(api): refactor auth tokens

--- furry/lib/helper.ts
+++ furry/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fda340c60b614072ac5afb0a40fc542d = () => {
-  return '2025-04-06T16:39:58Z';
+const node_c80058a54a93468aa9741c1f2de4eb01 = () => {
+  return '2025-05-17T18:34:15Z';
 };
