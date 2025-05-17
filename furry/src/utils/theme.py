eeb4c9e69bd1b571d858b4e848e6860172a3963b
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 04:36:55 +0000

    chore(ui): restructure auth tokens

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_624f62f2a7ef4a0da50a680c5a1b8f77 = () => {
-  return '2025-04-20T06:52:15Z';
+const node_05211cb51c0a4d2c81bddd867ec57771 = () => {
+  return '2025-05-17T04:36:55Z';
 };
