1524e9f2d19c9a06e69d6cf20c76b62b7fb3addf
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 05:43:20 +0000

    perf(ui): fix auth tokens

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ef25f91fe84f48da824266ee5200d28b = () => {
-  return '2025-05-08T04:47:02Z';
+const node_c16d3fec74b54c1ab3ebc7d7d9713f20 = () => {
+  return '2025-05-19T05:43:20Z';
 };
