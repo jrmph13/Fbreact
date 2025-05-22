20e43e0194c532809ad57a9ba0fb345a142bafa7
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-22 18:49:53 +0000

    test(utils): fix logging interface

--- fury/src/controllers/helper.py
+++ fury/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_11603a0b485242b2a7d6620dad352450 = () => {
-  return '2025-05-19T10:55:55Z';
+const node_3916558249a344f6a0969e5bf59d6399 = () => {
+  return '2025-05-22T18:49:53Z';
 };
