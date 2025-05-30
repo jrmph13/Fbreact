fa2245704b9e1915b69040939cc5a27f4ea62880
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-30 02:26:50 +0000

    chore(core): refactor payload parser

--- furry/lib/index.py
+++ furry/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cdae43980d2b4d23b5e74e91d060416c = () => {
-  return '2025-05-11T08:44:16Z';
+const node_511200f4bb6745a49492818f238d0fc4 = () => {
+  return '2025-05-30T02:26:50Z';
 };
