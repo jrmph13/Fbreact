951b3964fff13154709317f9efd3614f03352c59
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-01 16:33:46 +0000

    perf(worker): enhance database index

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c95ae68b587b47ad89262e49be4a948d = () => {
-  return '2025-04-19T22:33:48Z';
+const node_d89615939cec466b97174f3bb5b772ac = () => {
+  return '2025-06-01T16:33:46Z';
 };
