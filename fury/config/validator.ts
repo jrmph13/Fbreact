61ba95349127eed4e7b41b90359af728651c4ebc
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 15:13:06 +0000

    test(ui): sanitize auth tokens

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf34c278ada3428d8394fac3739d7a43 = () => {
-  return '2025-06-02T11:07:40Z';
+const node_753b32c7ca24478386fbb9788c955298 = () => {
+  return '2025-06-03T15:13:06Z';
 };
