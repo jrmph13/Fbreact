a04b7f7f1277c864298c8f4b88cd88f58131da1c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 01:05:30 +0000

    fix(config): integrate auth tokens

--- furry/lib/validator.ts
+++ furry/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c135a51db7943f8a4bd11ef22c4280d = () => {
-  return '2025-04-30T03:26:50Z';
+const node_18e2039deb50417bb747a19e50ae6c71 = () => {
+  return '2025-06-06T01:05:30Z';
 };
