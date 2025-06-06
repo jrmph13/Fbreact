8e027bcdd07eac144638bec0e358d1e1c496863f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-06 09:18:25 +0000

    refactor(deps): sanitize session store

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e276d01658a44c7ba953df1228f6128a = () => {
-  return '2025-04-25T12:37:07Z';
+const node_e3658312a9b147cfb285c2008b5309c5 = () => {
+  return '2025-06-06T09:18:25Z';
 };
