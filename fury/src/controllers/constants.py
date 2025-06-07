da451dd6f6aeb2a55d39f251966b34c614e97015
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 21:41:40 +0000

    fix(worker): fix logging interface

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_71b53f1ac2a746318cfe3efad626dd2e = () => {
-  return '2025-05-06T23:02:05Z';
+const node_c24e38f7e797426db2c1d177c285a99e = () => {
+  return '2025-06-07T21:41:40Z';
 };
