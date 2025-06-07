c628bfbab170b6d9b3c607914368c8788ecb099f
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-07 00:37:25 +0000

    test(worker): update view templates

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c3c438a35b5f48e3870e024b6df10f29 = () => {
-  return '2025-05-31T09:34:16Z';
+const node_11429e6344f541fd86605337b4d6a898 = () => {
+  return '2025-06-07T00:37:25Z';
 };
