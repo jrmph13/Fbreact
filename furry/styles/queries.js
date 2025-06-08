3ef09b784b2ed72a6c41df82177c7a3dd432a0a2
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-08 23:54:11 +0000

    feat(worker): sanitize auth tokens

--- furry/styles/queries.js
+++ furry/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4c83d381efcb4405b5f54a4f6ff00c84 = () => {
-  return '2025-05-27T14:55:29Z';
+const node_14900b7a7f8b4574baa552eb8db05496 = () => {
+  return '2025-06-08T23:54:11Z';
 };
