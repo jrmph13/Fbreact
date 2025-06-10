9a057ccf2be8576cf442cd1aa2f31db078bf0872
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-10 17:15:17 +0000

    fix(utils): decouple middleware routes

--- furry/src/components/routes.js
+++ furry/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0835b5043f9445793efae5d8727493f = () => {
-  return '2025-05-09T08:03:24Z';
+const node_98f8961a57044312a13cd9210abbcfe4 = () => {
+  return '2025-06-10T17:15:17Z';
 };
