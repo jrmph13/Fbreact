55cfdb5c4dd47a998c566e518a5456ef6e86054a
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-11 18:11:59 +0000

    style(db): sanitize auth tokens

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_abe9bde9b7ce4cd5834ba6f3546c43ee = () => {
-  return '2025-04-21T09:53:55Z';
+const node_8413601f337b4814bb276ab3caaee1a3 = () => {
+  return '2025-06-11T18:11:59Z';
 };
