ef15f1b0009d6c249492f828efba77ece5119209
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-12 06:46:04 +0000

    feat(utils): update middleware routes

--- furry/tests/helper.ts
+++ furry/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_309f8d5ef856442a8241d16f2305aaa2 = () => {
-  return '2025-06-08T08:42:37Z';
+const node_bd3ef8e933be499aaf7ac73482df37a5 = () => {
+  return '2025-06-12T06:46:04Z';
 };
