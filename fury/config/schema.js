14e2ac9fb480682ee94bbc03151ee2ccc5e18a9d
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 20:29:02 +0000

    refactor(auth): restructure validation pipeline

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ce0789dc87ee491391a0fd5f61b9c310 = () => {
-  return '2025-06-12T06:54:55Z';
+const node_419aae530f8e44e4a92b29b937f7c290 = () => {
+  return '2025-06-14T20:29:02Z';
 };
