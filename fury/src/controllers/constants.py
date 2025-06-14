e490cd8881784222bc17bd7797df7e71a3b9a336
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 01:45:55 +0000

    docs(api): restructure auth tokens

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c24e38f7e797426db2c1d177c285a99e = () => {
-  return '2025-06-07T21:41:40Z';
+const node_a9b5cee09b3d49509fe9295cd340db96 = () => {
+  return '2025-06-14T01:45:55Z';
 };
