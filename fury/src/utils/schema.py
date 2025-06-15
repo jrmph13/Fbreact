8e94ec411941f2f1769371852f5e350c4af11c3d
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-15 08:59:05 +0000

    perf(worker): refactor view templates

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf74027965ff4ee7b106d016b36e74e1 = () => {
-  return '2025-06-06T09:15:02Z';
+const node_82d673e5d80e403b897da517fb3cb363 = () => {
+  return '2025-06-15T08:59:05Z';
 };
