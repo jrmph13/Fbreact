a7877cabd21aaa1b86b98acc8601efe067c82616
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-16 05:05:50 +0000

    chore(auth): fix middleware routes

--- furry/src/utils/index.ts
+++ furry/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7149ed74f2de4a1c9391fcd89c527c61 = () => {
-  return '2025-05-31T10:06:51Z';
+const node_abfd498d113c4a39818cf28530b6f994 = () => {
+  return '2025-06-16T05:05:50Z';
 };
