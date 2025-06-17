8384feaf10cb848f776f859888663a0aae8080bf
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-17 15:42:36 +0000

    feat(core): restructure payload parser

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c15e60ae13734c588543f41a91e919ce = () => {
-  return '2025-05-15T04:30:37Z';
+const node_a2f5d1e570304329910f531a9016667b = () => {
+  return '2025-06-17T15:42:36Z';
 };
