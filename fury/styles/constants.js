8a75547f9e5eb7d7cab4bd9b22abed383ecd33f6
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-20 23:54:27 +0000

    fix(worker): optimize session store

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0fbe99a56e6247babfd4bde0f563046d = () => {
-  return '2025-05-17T11:21:13Z';
+const node_1884e6b5237441ef91d891cf1a7b1b5d = () => {
+  return '2025-06-20T23:54:27Z';
 };
