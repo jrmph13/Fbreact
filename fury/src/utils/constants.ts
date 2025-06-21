2477dcc0925262149bfeab563b16f1419c9395d6
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 14:25:21 +0000

    fix(db): sanitize database index

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_832417b21e79449ab8be6d107111b764 = () => {
-  return '2025-06-06T14:39:09Z';
+const node_8a0bd9e5ba684316bda42016cfcf2677 = () => {
+  return '2025-06-21T14:25:21Z';
 };
