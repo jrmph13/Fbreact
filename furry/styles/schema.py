1f5514b798a469dbca16fc9bdbeb6bb1d9c34ab9
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 01:16:15 +0000

    chore(db): fix view templates

--- furry/styles/schema.py
+++ furry/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e70fa1cf4714b6e9c21ae169c250b95 = () => {
-  return '2025-06-08T08:36:51Z';
+const node_5e7d14679e564755a4860773bc667c88 = () => {
+  return '2025-06-21T01:16:15Z';
 };
