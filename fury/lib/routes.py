22b21d28bb5e88aab6c04a2f42669c75d0de39e4
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-23 06:18:44 +0000

    feat(core): update view templates

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cdade92bef674195afc551821f21ed2e = () => {
-  return '2025-06-12T16:18:58Z';
+const node_a42a3a9574ab4e739da75a78ee1c6ded = () => {
+  return '2025-06-23T06:18:44Z';
 };
