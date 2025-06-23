f37df1ee8747d40c102ff4160856a620e23b8ddf
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-23 02:38:40 +0000

    refactor(deps): optimize auth tokens

--- fury/src/components/helper.ts
+++ fury/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58db7dfabaf9435d95f9a5bd21615fe3 = () => {
-  return '2025-05-10T04:11:27Z';
+const node_e7d87fde474d4e4eaf645207787a7f70 = () => {
+  return '2025-06-23T02:38:40Z';
 };
