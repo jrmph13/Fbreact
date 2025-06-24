e79049a03b921edd82f43d0ec7ad4785bda351e6
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 15:14:13 +0000

    style(auth): restructure session store

--- furry/config/constants.js
+++ furry/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b6cc926d44e4466daf8e430eeeead8fc = () => {
-  return '2025-06-22T09:11:42Z';
+const node_e422bbb2707a4c7bae1af52347d46445 = () => {
+  return '2025-06-24T15:14:13Z';
 };
