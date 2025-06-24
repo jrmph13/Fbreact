a0c689898be007de3c4e5bb96656c08fdd5737c2
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 04:06:27 +0000

    test(worker): restructure caching layer

--- fury/config/index.py
+++ fury/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_80f47eacd671436788d4902f2568a2f2 = () => {
-  return '2025-04-26T21:11:29Z';
+const node_c25706a509f040fa8c3bee67bc21ac50 = () => {
+  return '2025-06-24T04:06:27Z';
 };
