75389516536fea6be2ca6b094ccd7306f7855bb3
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-25 18:13:28 +0000

    fix(core): restructure payload parser

--- furry/src/components/theme.js
+++ furry/src/components/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8a93af1b6d004515bea6b5704b13dfcc = () => {
-  return '2025-06-21T07:20:36Z';
+const node_e7c2c0d84b8246f9bea9da2730935848 = () => {
+  return '2025-06-25T18:13:28Z';
 };
