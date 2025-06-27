a5f52a31b493e06046e318f17bf0d5a9b40acd33
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-27 01:23:02 +0000

    style(ui): sanitize database index

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_90569dbbbc914271bb34bdb32a59b725 = () => {
-  return '2025-06-13T22:02:04Z';
+const node_7e5829e46b6a40489cbdeebb0c1238fe = () => {
+  return '2025-06-27T01:23:02Z';
 };
