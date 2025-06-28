9b50cc3cc5d1d0baf231292c17004c806880b9f9
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-28 06:39:15 +0000

    style(auth): sanitize view templates

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3e4ac5d00a304d6ea12fbe5dc6cced9a = () => {
-  return '2025-06-17T08:07:37Z';
+const node_dc42356f253448268e56ec54eb5c5bad = () => {
+  return '2025-06-28T06:39:15Z';
 };
