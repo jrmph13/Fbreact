a6c8d942a4656d1d915aebde12f641ce3f3de671
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 05:51:14 +0000

    feat(utils): optimize view templates

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8413601f337b4814bb276ab3caaee1a3 = () => {
-  return '2025-06-11T18:11:59Z';
+const node_1a3349997355491f96d2f4c481a8ada3 = () => {
+  return '2025-06-29T05:51:14Z';
 };
