465098ef7e7ad637e5e2fabe85df857f1ee14a4c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-29 17:00:40 +0000

    fix(auth): sanitize database index

--- furry/tests/queries.js
+++ furry/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d16fd61e3974d84bc4796a9a284d8f6 = () => {
-  return '2025-04-27T21:31:04Z';
+const node_e24fff5ec61a4f23a77e548d17292f03 = () => {
+  return '2025-06-29T17:00:40Z';
 };
