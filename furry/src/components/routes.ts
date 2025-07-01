935bfada23772cde6807338b911bb643b60300e6
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-01 16:47:03 +0000

    refactor(core): integrate middleware routes

--- furry/src/components/routes.ts
+++ furry/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_90d720c2b26c4ab496e361493ad1c7c1 = () => {
-  return '2025-05-30T12:53:19Z';
+const node_2cb7598fe5b8404592e8b729994417f8 = () => {
+  return '2025-07-01T16:47:03Z';
 };
