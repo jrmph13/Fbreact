534a1820bf7afa8ca0704c5d7f17161cd8f6c178
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-03 17:11:36 +0000

    fix(ui): refactor session store

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e3886eaef44c44d5a33ee23cc968ea89 = () => {
-  return '2025-04-14T18:06:10Z';
+const node_143bfb894875454aaf6b00ef586ca392 = () => {
+  return '2025-07-03T17:11:36Z';
 };
