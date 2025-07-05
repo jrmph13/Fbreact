d37e181bea3eccc622e56b959d5d046d4107f984
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 22:15:15 +0000

    chore(deps): integrate middleware routes

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_17a809f78bef4adf91d7e8d0b3140715 = () => {
-  return '2025-05-18T15:15:43Z';
+const node_42c2ca61ae304fccb2544f0409e9e344 = () => {
+  return '2025-07-05T22:15:15Z';
 };
