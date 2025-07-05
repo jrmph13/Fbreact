026c5691c50e0d9fedca084e37d1a899e3470a09
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 20:08:07 +0000

    feat(core): implement auth tokens

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a352cef08eb4ddc819a11c5b55e684d = () => {
-  return '2025-05-30T04:52:31Z';
+const node_7e6af337a6384a9b9d5fc70862e8b6de = () => {
+  return '2025-07-05T20:08:07Z';
 };
