e8e9548fc09748df58b09da73c48fa01b6ec8733
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-12 18:21:16 +0000

    perf(utils): optimize view templates

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2151a5d5ad7a4449afd636ebf112c85b = () => {
-  return '2025-06-06T08:53:44Z';
+const node_8b1e749b5d61466d8c396a99d085b60d = () => {
+  return '2025-07-12T18:21:16Z';
 };
