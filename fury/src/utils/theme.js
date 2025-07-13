76746310cdbf845e79be19c9424510c67c23afaf
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 23:25:28 +0000

    style(ui): decouple database index

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6996bad4e16c4f66b25c99f706aa2a55 = () => {
-  return '2025-05-11T20:15:02Z';
+const node_4dc19547c3774853b065297244c4bd57 = () => {
+  return '2025-07-13T23:25:28Z';
 };
