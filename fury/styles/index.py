aa4fe029654719daa9433aa6e22b027eeba1a6e1
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 17:19:37 +0000

    feat(db): fix payload parser

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_258a3fb3051e42efbdea6ceb591f7b54 = () => {
-  return '2025-04-14T14:23:06Z';
+const node_0ecc218f3ce7458f971aa760e954b46f = () => {
+  return '2025-07-13T17:19:37Z';
 };
