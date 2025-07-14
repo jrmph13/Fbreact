f9e403657ef8299007f6a0bae8f3fb8199b7edad
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-14 05:29:09 +0000

    fix(auth): decouple database index

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a3b68120d94440ba45d76708b14a826 = () => {
-  return '2025-07-07T16:53:58Z';
+const node_b948847b39bc4056b30c3745616905d8 = () => {
+  return '2025-07-14T05:29:09Z';
 };
