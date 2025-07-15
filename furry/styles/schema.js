da2964e0d98292c7c766e4b2cd9d73f12cf8f0e5
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 07:55:22 +0000

    feat(api): refactor auth tokens

--- furry/styles/schema.js
+++ furry/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5032c2ddb3ca4711b803fb13f97d8c9e = () => {
-  return '2025-04-15T18:10:16Z';
+const node_b4c1ea8617e149c1b41e383fc06ecfe9 = () => {
+  return '2025-07-15T07:55:22Z';
 };
