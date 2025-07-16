9b737dc2b1b2f275f96aba2967eb965e69e642a1
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-16 08:04:37 +0000

    chore(ui): refactor database index

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c411bbcde9f24072991d935813d3044b = () => {
-  return '2025-06-16T12:32:06Z';
+const node_a3c505542e1c4764a0761861d51e4c68 = () => {
+  return '2025-07-16T08:04:37Z';
 };
