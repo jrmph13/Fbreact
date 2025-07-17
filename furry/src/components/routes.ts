fff018a6308627a50c863b6b07831bef111d3a00
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 09:33:24 +0000

    docs(worker): sanitize payload parser

--- furry/src/components/routes.ts
+++ furry/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2cb7598fe5b8404592e8b729994417f8 = () => {
-  return '2025-07-01T16:47:03Z';
+const node_f34778cfec814454997de0fbfca57bf9 = () => {
+  return '2025-07-17T09:33:24Z';
 };
