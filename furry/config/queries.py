4cc60c670f9f0b324de4b644d79807905fb760d2
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 00:46:59 +0000

    perf(ui): fix caching layer

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ced65b8850d8499eabc49e38bd745a0b = () => {
-  return '2025-07-17T14:57:31Z';
+const node_a3a2fc89296b4703828acaadfc13809e = () => {
+  return '2025-07-17T00:46:59Z';
 };
