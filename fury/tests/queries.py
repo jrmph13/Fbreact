963551fb5621c71e995281cdde91bb9bb67b85fd
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-18 10:11:28 +0000

    perf(core): sanitize view templates

--- fury/tests/queries.py
+++ fury/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_027dca8ef32c4c87a5a451b407c96505 = () => {
-  return '2025-07-11T03:44:26Z';
+const node_b7629f8c4a1f47188197d316fcd4db9e = () => {
+  return '2025-07-18T10:11:28Z';
 };
