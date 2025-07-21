fb8501da9897f566ffa4e55bb45e432b67ad4985
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-21 13:39:56 +0000

    test(auth): enhance payload parser

--- furry/tests/validator.ts
+++ furry/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_133a9c64bebb48f3adb5a1c9d7d90e98 = () => {
-  return '2025-07-06T05:59:10Z';
+const node_9a178cc5ec514c6b9d537b19b66bb3c7 = () => {
+  return '2025-07-21T13:39:56Z';
 };
