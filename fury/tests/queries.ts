cf2b4fe87edf1de37e895d9e1160699b5f8562cf
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 20:56:29 +0000

    test(worker): decouple middleware routes

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a4010755b74c4cda962b480650ee7fbe = () => {
-  return '2025-07-11T18:34:18Z';
+const node_2ab2df3dc5ed4a61a0ff004a79ed37c6 = () => {
+  return '2025-07-28T20:56:29Z';
 };
