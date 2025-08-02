12dd2e889dfa702f0f7a8d6305bdb7c2f691e502
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 12:43:47 +0000

    fix(api): decouple view templates

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14e73ca5dc00419caf1ff8381e4da8a1 = () => {
-  return '2025-08-02T11:52:04Z';
+const node_ea97f0fc002d440c93848928c2fc3fee = () => {
+  return '2025-08-02T12:43:47Z';
 };
