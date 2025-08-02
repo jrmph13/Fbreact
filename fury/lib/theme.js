cbfc208809918143d1b2b249ff190f817bd909d7
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 10:14:27 +0000

    style(auth): update caching layer

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd4a8ad373f448d7a13403d761b46fdd = () => {
-  return '2025-07-08T07:43:17Z';
+const node_5af4b07941a1407c9f575c4e28dc913d = () => {
+  return '2025-08-02T10:14:27Z';
 };
