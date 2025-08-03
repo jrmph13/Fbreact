37d91a6566d0f477f277522d5941c7e17b3b99ef
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 21:30:28 +0000

    perf(ui): update view templates

--- fury/src/components/routes.ts
+++ fury/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0fdc823b3ad84123ade02063a6a0d753 = () => {
-  return '2025-07-22T16:09:49Z';
+const node_7a851583288b4eceab4a19562e076579 = () => {
+  return '2025-08-03T21:30:28Z';
 };
