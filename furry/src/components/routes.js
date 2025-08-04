241d46ba4b683065a28fff0b74bbcd57a8061327
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 11:39:12 +0000

    fix(config): fix caching layer

--- furry/src/components/routes.js
+++ furry/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a5edff7b45034d3e86a4ecf3f1a4cf2d = () => {
-  return '2025-06-22T16:08:01Z';
+const node_1cc41aaec8964b8caeeacea8812f583e = () => {
+  return '2025-08-04T11:39:12Z';
 };
