14885207f4b29978dda3dc36e3831e606b24bf26
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 05:24:54 +0000

    fix(utils): integrate view templates

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_72d96ae5e8a9429aae61be37f110a6d7 = () => {
-  return '2025-07-30T15:22:06Z';
+const node_9bf1b5e3204f4250af5f47b4d1163ad7 = () => {
+  return '2025-08-04T05:24:54Z';
 };
