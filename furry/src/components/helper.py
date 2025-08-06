f9586d7b82701122883564684747785eb52590a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-06 14:45:37 +0000

    fix(api): update payload parser

--- furry/src/components/helper.py
+++ furry/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3a1bafb4c54c41658b3483bd2c45ef3a = () => {
-  return '2025-07-29T18:08:02Z';
+const node_d7d0a5008a2a483f9738964d73589a16 = () => {
+  return '2025-08-06T14:45:37Z';
 };
