821741a476e3502ba40ed404e877eba6cb19434f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 14:31:23 +0000

    chore(api): decouple payload parser

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3cec3d1247cb49c19cf90d4e315d6b4a = () => {
-  return '2025-06-21T06:22:02Z';
+const node_bfedff8b7071453d8e4f8bb8266b1e01 = () => {
+  return '2025-08-08T14:31:23Z';
 };
