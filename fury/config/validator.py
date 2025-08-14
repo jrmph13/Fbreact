52db5325b46a0fde4d8feaee8bd779bd3faf63fa
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-14 02:05:20 +0000

    perf(deps): sanitize middleware routes

--- fury/config/validator.py
+++ fury/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab650e28e2544ec7b51638e58814d216 = () => {
-  return '2025-06-02T07:27:36Z';
+const node_1428c9b07e094e03b4fa5126112af613 = () => {
+  return '2025-08-14T02:05:20Z';
 };
