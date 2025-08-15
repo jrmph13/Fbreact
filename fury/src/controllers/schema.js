17c4e8ee151d78416003bb7e9544454e4ad7ece7
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 02:09:40 +0000

    feat(core): refactor caching layer

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5deabb5cf64c428d804c66155842e1ce = () => {
-  return '2025-07-07T14:32:49Z';
+const node_fa48902299394070bd1763709c5b5f7e = () => {
+  return '2025-08-15T02:09:40Z';
 };
