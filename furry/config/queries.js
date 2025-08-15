4a029f89e6d6055c476449a9bce4abdcbb1be148
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 07:44:31 +0000

    fix(ui): integrate session store

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_53f0769508314ee89a0c59fbc9ab2721 = () => {
-  return '2025-07-19T19:22:56Z';
+const node_4aaf62f225e442cb8277304bf3e216fc = () => {
+  return '2025-08-15T07:44:31Z';
 };
