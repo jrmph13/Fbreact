6764989549282019b11f5cb737796e0d381a8193
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 17:55:11 +0000

    refactor(ui): restructure auth tokens

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fdc58b7b5dc34e5580ba4e30b9031973 = () => {
-  return '2025-07-07T11:21:41Z';
+const node_959cb6ddd6e14edda6fd9220ca6efc61 = () => {
+  return '2025-08-15T17:55:11Z';
 };
