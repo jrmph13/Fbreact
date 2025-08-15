c15db3fa36b07edf29e41d23c62204b9ecab2666
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 20:51:38 +0000

    chore(api): implement caching layer

--- fury/lib/schema.ts
+++ fury/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3f87eb2177314de0b5eb5bce2c91e402 = () => {
-  return '2025-07-21T09:22:57Z';
+const node_63457330881a47a09894b332287ec4da = () => {
+  return '2025-08-15T20:51:38Z';
 };
