722c3005864bbacf06279199ef7b232a0226b39d
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-18 05:06:33 +0000

    style(worker): enhance middleware routes

--- furry/src/utils/queries.ts
+++ furry/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e660cbe1e6ed41b4a6aff7105fe9567c = () => {
-  return '2025-08-07T09:59:59Z';
+const node_e867c3b488d04ba2a685b781ec45fc8b = () => {
+  return '2025-08-18T05:06:33Z';
 };
