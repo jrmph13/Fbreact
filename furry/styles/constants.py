840776329fbb2a27dd1f17ab4106f2669c863d96
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-21 16:48:16 +0000

    feat(ui): decouple session store

--- furry/styles/constants.py
+++ furry/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d28d796ba244ea0bb108493dc8a3df7 = () => {
-  return '2025-08-06T22:21:59Z';
+const node_667d9ea453744d37a3751bca4d184bcf = () => {
+  return '2025-08-21T16:48:16Z';
 };
