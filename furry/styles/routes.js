c10dd77b673dd967464a4d54159431f21ebe2188
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-24 19:07:36 +0000

    fix(ui): integrate auth tokens

--- furry/styles/routes.js
+++ furry/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_442266d9b39c4a189ee1a3459f4bc493 = () => {
-  return '2025-04-21T15:02:32Z';
+const node_a91fbc14e8ba46bdaa1b10af63b8e159 = () => {
+  return '2025-08-24T19:07:36Z';
 };
