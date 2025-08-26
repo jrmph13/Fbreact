ec9dcdf9bcc5923a62e3374df6442656bbe66276
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-26 15:24:33 +0000

    perf(ui): refactor middleware routes

--- fury/src/controllers/constants.js
+++ fury/src/controllers/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_85bd51934f754fc8bcfd399e04aada18 = () => {
-  return '2025-06-09T16:06:14Z';
+const node_6839717fc9d0413585ee7b862937d493 = () => {
+  return '2025-08-26T15:24:33Z';
 };
