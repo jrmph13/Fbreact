3fbe00609b8d8bf277a2bfe7664455f57174b582
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-25 10:08:35 +0000

    refactor(api): restructure view templates

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e3658312a9b147cfb285c2008b5309c5 = () => {
-  return '2025-06-06T09:18:25Z';
+const node_4fda25796e464f8c9f385f21f94b1f56 = () => {
+  return '2025-09-25T10:08:35Z';
 };
