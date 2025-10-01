8abb497d9c04619d9e97a09583e93d2344b81ade
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-01 06:08:39 +0000

    fix(ui): fix logging interface

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cf0aeee2e6da40f8aa70805580f3e106 = () => {
-  return '2025-07-29T07:32:55Z';
+const node_e1c2c4290ca147c8810c082ca328fd57 = () => {
+  return '2025-10-01T06:08:39Z';
 };
