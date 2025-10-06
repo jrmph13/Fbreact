8f5c30f4733bb0362dbcaac42dce55e0d1f7da46
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-06 11:49:53 +0000

    chore(ui): refactor caching layer

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4315858b633449c395dc4bb45f931cab = () => {
-  return '2025-07-13T19:18:33Z';
+const node_a10808ab45484dac879357c1161e1032 = () => {
+  return '2025-10-06T11:49:53Z';
 };
