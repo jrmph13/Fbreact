45903419b89ec7bf55dddef0e80dcf231a1921b9
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-19 00:40:15 +0000

    perf(auth): optimize payload parser

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb61f498cd6b421d90d3a9ef51295d6b = () => {
-  return '2025-10-13T14:34:24Z';
+const node_91f9a819bff5439cbcbdda1123f64ba2 = () => {
+  return '2025-10-19T00:40:15Z';
 };
