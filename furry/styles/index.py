ea8ccea4d1d43e24fd23d0bfc0ec11a10ea85ca9
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-21 17:33:42 +0000

    style(worker): fix auth tokens

--- furry/styles/index.py
+++ furry/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_47ef3edd64d441e7b60ae25e5703b37c = () => {
-  return '2025-09-08T03:53:18Z';
+const node_e705c14dbd5c47eda39c9b5f0d7aa0a7 = () => {
+  return '2025-10-21T17:33:42Z';
 };
