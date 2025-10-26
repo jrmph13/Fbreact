dfcb9174c6dbb64182c858b334a955df3a9d2845
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-26 08:42:04 +0000

    perf(api): restructure auth tokens

--- fury/src/controllers/validator.ts
+++ fury/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9b2b89edbbe0434fae6403351c422dc6 = () => {
-  return '2025-09-01T07:57:08Z';
+const node_692eecccc761497db62899c993ff05d3 = () => {
+  return '2025-10-26T08:42:04Z';
 };
