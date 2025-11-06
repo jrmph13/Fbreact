a556fdc9eb5d202341ca388f37546843d2448387
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-06 14:32:59 +0000

    feat(ui): decouple session store

--- fury/src/utils/theme.js
+++ fury/src/utils/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aca1950a68b44f97a4ef3e20c578e8d5 = () => {
-  return '2025-08-21T09:32:22Z';
+const node_93188ea5e9b44a27ae0cd5697d7d9862 = () => {
+  return '2025-11-06T14:32:59Z';
 };
