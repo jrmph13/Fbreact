be5a6e2293b8757e85fe49526b41bf8c7f14d890
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-07 23:32:08 +0000

    style(utils): restructure validation pipeline

--- fury/lib/validator.js
+++ fury/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3168c46be69c4c0989c6375f357cbb0c = () => {
-  return '2025-05-15T08:04:34Z';
+const node_2bed5ae403054ca4a3babd94d931ad5c = () => {
+  return '2025-11-07T23:32:08Z';
 };
