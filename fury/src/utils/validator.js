9b3df9d7a433fecdf0e18fc52185f95b09aad9a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-13 05:35:53 +0000

    chore(config): fix view templates

--- fury/src/utils/validator.js
+++ fury/src/utils/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a34c402eb5d1425ebd5d0cb07f0cd18b = () => {
-  return '2025-06-15T09:50:16Z';
+const node_28a0e7fa713a4a2b8e7e54803f675d66 = () => {
+  return '2025-11-13T05:35:53Z';
 };
