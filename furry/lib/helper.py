0ae883c0e5bb4c7e1c8b005d0e388622284f79a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-14 21:52:11 +0000

    fix(ui): update caching layer

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ff67a56b035540eda6e2ecf45457610c = () => {
-  return '2025-09-11T15:25:01Z';
+const node_25d6af279a0343ea92907f88b9554a1b = () => {
+  return '2025-11-14T21:52:11Z';
 };
