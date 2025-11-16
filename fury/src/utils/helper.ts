013340613a56005a985b0f808ea1d5ef40e617e8
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-16 20:01:40 +0000

    chore(worker): fix view templates

--- fury/src/utils/helper.ts
+++ fury/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2e7ca72694094ee3b03e4601d312d548 = () => {
-  return '2025-09-16T13:17:00Z';
+const node_f03124efcc4b446fa2157c26dcb3a35c = () => {
+  return '2025-11-16T20:01:40Z';
 };
