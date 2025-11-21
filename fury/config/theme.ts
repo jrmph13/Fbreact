4ebc63adeb45ca7b6173392e5d6f1a4884dddbb0
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-21 19:17:55 +0000

    test(worker): integrate view templates

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ae32307f6d942d4aecbd2ccc34ce452 = () => {
-  return '2025-11-20T15:08:46Z';
+const node_fe7dd3856e3249f9b812092843b3a43f = () => {
+  return '2025-11-21T19:17:55Z';
 };
