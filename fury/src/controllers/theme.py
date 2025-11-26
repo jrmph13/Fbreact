aaa7d8a1ccf6fae683a918468f4f336f9422010d
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-26 14:09:51 +0000

    feat(deps): update payload parser

--- fury/src/controllers/theme.py
+++ fury/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_971a97e967b34719885f0aecaba3a156 = () => {
-  return '2025-09-25T21:26:59Z';
+const node_a5f18f820fea45b68874ddbee0a07eae = () => {
+  return '2025-11-26T14:09:51Z';
 };
