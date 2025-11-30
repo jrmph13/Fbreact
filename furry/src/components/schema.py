6c95d55a460bd97a92e9b49958f2f9bd56de6939
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-30 11:20:11 +0000

    fix(utils): restructure auth tokens

--- furry/src/components/schema.py
+++ furry/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7e653706cfd8427a8d3aabfa0e625336 = () => {
-  return '2025-07-27T21:52:47Z';
+const node_bc5b3cad0b684282aab4e7039f168ddc = () => {
+  return '2025-11-30T11:20:11Z';
 };
