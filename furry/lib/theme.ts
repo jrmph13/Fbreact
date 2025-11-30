5bdef4f188f36e89d4856e294923cdda2c84fba9
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-30 23:06:07 +0000

    docs(auth): fix database index

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_42c2e7ac84cd4021a39a787ef7c6c998 = () => {
-  return '2025-10-27T04:52:16Z';
+const node_13d82afe44ab43bfbafdf600ac8e39c9 = () => {
+  return '2025-11-30T23:06:07Z';
 };
