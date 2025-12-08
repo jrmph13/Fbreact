c3b0542da0f22d0dfe20305be8b68f6af7f934ee
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-08 11:33:12 +0000

    docs(utils): enhance middleware routes

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b53b358a2ab048208ad2967fe3742fe4 = () => {
-  return '2025-08-05T09:45:16Z';
+const node_5494312b92f84c61a1b79105b82348f9 = () => {
+  return '2025-12-08T11:33:12Z';
 };
