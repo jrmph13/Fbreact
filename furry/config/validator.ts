f69e9c861af491a956ae1c3090628c4fd089bf0a
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-11 23:20:49 +0000

    chore(auth): update session store

--- furry/config/validator.ts
+++ furry/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_919060df3463491cb7e6890fd2c61091 = () => {
-  return '2025-08-20T18:05:58Z';
+const node_05bf1bdeb4634751b2bf7f14f1fde006 = () => {
+  return '2025-12-11T23:20:49Z';
 };
