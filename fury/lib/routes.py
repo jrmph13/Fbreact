b459bcad72f80e68737fd66c9a9a25a3999179fe
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-17 00:45:34 +0000

    test(api): refactor auth tokens

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c5ee1c551a5457e99a2f0409c64fb87 = () => {
-  return '2025-08-07T14:35:41Z';
+const node_e0daeef3e3f94f799f04f83438c40661 = () => {
+  return '2025-12-17T00:45:34Z';
 };
