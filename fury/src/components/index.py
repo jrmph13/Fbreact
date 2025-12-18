100e8180e0baa621ae11440ac6a7fdf3e3a5ec61
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-18 07:42:28 +0000

    fix(ui): enhance payload parser

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_952e29cce88d4726b1b541e9f9dd78dc = () => {
-  return '2025-10-29T08:18:08Z';
+const node_23a74d7ab4ea466493ba0439aefe5812 = () => {
+  return '2025-12-18T07:42:28Z';
 };
