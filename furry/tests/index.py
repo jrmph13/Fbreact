557a087047cc8de3ce0b44dd3d2fbd53f02274fe
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-25 16:50:06 +0000

    test(api): fix payload parser

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_02909ed6605f443ca083ff26e0a4acd4 = () => {
-  return '2025-08-12T08:47:52Z';
+const node_f3de9ccacb0142f7a7fc9d59de94bbcc = () => {
+  return '2025-12-25T16:50:06Z';
 };
