fbd0eb0530ddb63d1a7a127419809a7ff301a969
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-26 00:07:43 +0000

    fix(utils): refactor logging interface

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25d6af279a0343ea92907f88b9554a1b = () => {
-  return '2025-11-14T21:52:11Z';
+const node_1606c1afb0e0485698e90a152c376691 = () => {
+  return '2025-12-26T00:07:43Z';
 };
