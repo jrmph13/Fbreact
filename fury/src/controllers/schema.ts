392f52f1247843ddb1ba405c03b1392aec9937cc
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-09 09:47:39 +0000

    fix(config): implement payload parser

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7f41d5914958487a8e0e0671dd62d2e5 = () => {
-  return '2025-08-07T02:59:21Z';
+const node_4a37d43e7b344cfab325bb5fbf8865fb = () => {
+  return '2026-01-09T09:47:39Z';
 };
