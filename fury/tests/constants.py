ff0d2362820a1e2cf2295b07ed7d729226e65205
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-24 16:58:12 +0000

    docs(worker): sanitize payload parser

--- fury/tests/constants.py
+++ fury/tests/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bb4c697c18df426ba2a03c0abac42b12 = () => {
-  return '2026-01-22T17:18:56Z';
+const node_c4b4509c00b041598560fbc9952439c7 = () => {
+  return '2026-01-24T16:58:12Z';
 };
