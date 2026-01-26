1b2bcd3b3a3c50003d6a1f06e3001c8542f9710c
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-26 09:49:00 +0000

    test(deps): restructure payload parser

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21c6e8df3eda4cf5b608b6a24508fa6f = () => {
-  return '2026-01-14T01:45:07Z';
+const node_25a06ab752094041b86beb922a0af6f3 = () => {
+  return '2026-01-26T09:49:00Z';
 };
