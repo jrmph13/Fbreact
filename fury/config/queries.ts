de5c557999a018768d85368316cdbfbea9050bd5
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-27 07:31:13 +0000

    feat(api): sanitize view templates

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_47b88accfd1e4484aa61a91ab452890b = () => {
-  return '2025-09-08T09:38:16Z';
+const node_7335ee12ed8b44edad4ae0ec9e1a6dec = () => {
+  return '2026-01-27T07:31:13Z';
 };
