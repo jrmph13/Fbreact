765369100de6a0e9706ef86cbd2b70f261040959
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-29 17:53:49 +0000

    perf(auth): enhance auth tokens

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2d17428909ba422db86a98adff8238a0 = () => {
-  return '2025-04-20T03:48:06Z';
+const node_425ac26afbd64cb5a81c121e75676604 = () => {
+  return '2026-01-29T17:53:49Z';
 };
