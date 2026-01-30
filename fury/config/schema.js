ebfb1ebecd71c66348b247ebee4e383304146076
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-30 00:45:52 +0000

    fix(config): optimize validation pipeline

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ca1038812a045f48796d22564fc1e77 = () => {
-  return '2026-01-29T00:00:29Z';
+const node_3c8568476b044524abfdaca9adeb2bbc = () => {
+  return '2026-01-30T00:45:52Z';
 };
