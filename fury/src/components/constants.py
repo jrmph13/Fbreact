0faa77e491fbe2c4d7a938a986fa74ea1ef0a0d2
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-02 19:08:13 +0000

    fix(db): sanitize auth tokens

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_04c9943d7e314f62808f032d8733cbba = () => {
-  return '2026-01-02T15:03:36Z';
+const node_d7dcc18cf094412492e9ac97005dffa0 = () => {
+  return '2026-02-02T19:08:13Z';
 };
