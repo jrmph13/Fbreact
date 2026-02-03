af45b6bf534912391ba8196b9dbee76bc47c2588
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-03 07:56:33 +0000

    fix(deps): integrate database index

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b3f49482582246cbbca41959447dfea9 = () => {
-  return '2026-01-07T13:04:24Z';
+const node_ccc567f933b04a3f8ee28ec2a017af07 = () => {
+  return '2026-02-03T07:56:33Z';
 };
