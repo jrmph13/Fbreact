bdcd61032b526f9a96aadcc4ca727151b0774a27
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-10 16:45:50 +0000

    chore(auth): sanitize session store

--- furry/src/controllers/routes.js
+++ furry/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f3aa70781a0d4b7eb184873270b440ce = () => {
-  return '2025-06-23T15:21:19Z';
+const node_6c4a1a1e663b487aa68565f333fd1000 = () => {
+  return '2026-02-10T16:45:50Z';
 };
