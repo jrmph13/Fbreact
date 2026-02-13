ea061fca1b2a8d19fbf29def192cb34a8187f862
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-13 23:41:23 +0000

    chore(ui): decouple payload parser

--- fury/lib/queries.py
+++ fury/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b31583fecaf42c7b02d2ad21de71089 = () => {
-  return '2025-12-10T15:02:36Z';
+const node_dc65a75356f94764933197032374a14f = () => {
+  return '2026-02-13T23:41:23Z';
 };
