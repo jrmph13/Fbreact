2c07f477a7028badb3c9f7d08bbbab49e5fe488f
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 17:08:03 +0000

    fix(api): optimize view templates

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c1580322fd144a2979f1e66f628bc35 = () => {
-  return '2026-02-10T01:03:06Z';
+const node_c7ecb062d92049f7b2efdfd0047e8af3 = () => {
+  return '2026-02-17T17:08:03Z';
 };
