21a34b9ce30fc2a6ad2b1724dc05205e436a0fb6
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 08:52:45 +0000

    docs(auth): restructure session store

--- furry/src/components/queries.js
+++ furry/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_30817436b6174e90a2ce91149a74ffaf = () => {
-  return '2025-08-12T23:47:00Z';
+const node_7f4d16a0731e4f96aec05cd67f9ad796 = () => {
+  return '2026-02-17T08:52:45Z';
 };
