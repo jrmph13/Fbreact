04ce9109fad6ec01ea69d3037e3c91c09aba6980
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-18 21:24:59 +0000

    fix(worker): enhance caching layer

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ac8bfd868764086a89f1588fc0c6589 = () => {
-  return '2026-02-14T00:46:59Z';
+const node_400e8b358a6245e0b749999220416b08 = () => {
+  return '2026-02-18T21:24:59Z';
 };
