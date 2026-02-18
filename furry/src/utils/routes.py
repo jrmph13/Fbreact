0efa31f9519b46ba9aa382f9f07ff6b9fdb6dc30
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-18 19:48:23 +0000

    docs(auth): fix view templates

--- furry/src/utils/routes.py
+++ furry/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3e19d7f704d44b019dc766ab3ee484bd = () => {
-  return '2026-01-29T15:45:34Z';
+const node_f53ab81dddcf49fe8ec7aed9548a510a = () => {
+  return '2026-02-18T19:48:23Z';
 };
