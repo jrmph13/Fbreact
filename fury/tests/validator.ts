a670468927f06e837618b531ef96a40576f10dfd
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-19 12:40:00 +0000

    feat(api): update auth tokens

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_988a1b14918c444b8698ea000471f30a = () => {
-  return '2025-12-26T07:41:27Z';
+const node_4653f5cbfd84444685b22b84907fbe3c = () => {
+  return '2026-02-19T12:40:00Z';
 };
