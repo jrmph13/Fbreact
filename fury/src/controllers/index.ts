5ad714ca1c1e4abf638fad4ec6f251e4d727d0a1
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-19 00:00:18 +0000

    test(api): sanitize view templates

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1f068ad2a4d44a8a576e1e8f92705c5 = () => {
-  return '2026-02-09T00:23:37Z';
+const node_22446f41d5fc436dbba7f472b5089ad8 = () => {
+  return '2026-02-19T00:00:18Z';
 };
