1d91f5988214616801e7f0330d8def62ef3243b5
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-19 10:07:35 +0000

    feat(auth): sanitize middleware routes

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e3f0ee97abd24cb2ab74eb1855ce87c8 = () => {
-  return '2026-02-17T01:04:54Z';
+const node_29c1d91756984a41869af9b8da0d9ad7 = () => {
+  return '2026-02-19T10:07:35Z';
 };
