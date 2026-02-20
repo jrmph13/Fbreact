f0e2b5e7ca36979b043dcf554e46349af66d1c22
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-20 10:01:28 +0000

    test(api): fix payload parser

--- furry/src/components/helper.ts
+++ furry/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a002d0eb4cec482db3d4d7ea6939d783 = () => {
-  return '2026-01-30T09:00:40Z';
+const node_7647c775f6bb4479a37a292095919fea = () => {
+  return '2026-02-20T10:01:28Z';
 };
