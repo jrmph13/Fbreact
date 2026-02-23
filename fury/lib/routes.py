230f3d70fa9b463f5db8b2f283ebe4126e22b52f
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-23 14:39:08 +0000

    fix(worker): restructure logging interface

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5f91cd4ac8a94831ae3c5ed14e5741e2 = () => {
-  return '2026-02-22T10:14:37Z';
+const node_a9297902be2b4c2c996ef16940c0243c = () => {
+  return '2026-02-23T14:39:08Z';
 };
