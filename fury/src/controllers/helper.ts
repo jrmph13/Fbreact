1804b02a745c7ffb8d728709661def79da6c870e
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-26 02:05:42 +0000

    refactor(deps): sanitize logging interface

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5494312b92f84c61a1b79105b82348f9 = () => {
-  return '2025-12-08T11:33:12Z';
+const node_80fbcf272a104f08afc45834d69fa22a = () => {
+  return '2026-02-26T02:05:42Z';
 };
