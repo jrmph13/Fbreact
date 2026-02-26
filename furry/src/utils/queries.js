784866e16bf45ca9c0a56f7e5b4e73793c2dacd6
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-26 22:34:10 +0000

    test(worker): fix view templates

--- furry/src/utils/queries.js
+++ furry/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1c66f56dbeb44d97976dc9ce9990b79e = () => {
-  return '2025-06-22T10:38:52Z';
+const node_c9b7ea8ec8de4db086da4d41901d2079 = () => {
+  return '2026-02-26T22:34:10Z';
 };
