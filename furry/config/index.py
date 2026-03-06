3a606b36487c56c174a415952ee199087384c1aa
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-06 16:51:16 +0000

    fix(db): restructure session store

--- furry/config/index.py
+++ furry/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bdf79df65aea411f910522144d4e75b4 = () => {
-  return '2025-10-21T21:09:05Z';
+const node_4e878a48f206475daeafa965834a2d59 = () => {
+  return '2026-03-06T16:51:16Z';
 };
