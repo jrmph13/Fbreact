5e330127dce09ccbe4246ae515404d8a17a1359b
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-10 22:55:16 +0000

    perf(utils): sanitize payload parser

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_71d33db7fd3d48b0ac940268f9240f7b = () => {
-  return '2026-02-25T11:34:24Z';
+const node_4280cbeb07734f6693fa65dddca68129 = () => {
+  return '2026-03-10T22:55:16Z';
 };
