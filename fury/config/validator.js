7a19344c985c96cb7a04a02902dced55f6a75903
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-15 12:12:02 +0000

    perf(auth): optimize middleware routes

--- fury/config/validator.js
+++ fury/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_dfd79b0508514ae7bbfdb694a62eb3c8 = () => {
-  return '2026-01-29T19:16:32Z';
+const node_951c61852d554b89b983f0c310f031f2 = () => {
+  return '2026-03-15T12:12:02Z';
 };
