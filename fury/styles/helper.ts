a5c60748a1707a115b7e2f9c34335b1fc1e379cc
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-22 23:35:36 +0000

    test(db): fix session store

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9e7af5f3fa3f4c79b33a7045f3a4caa5 = () => {
-  return '2026-02-20T05:58:06Z';
+const node_cf431309cd91405883000ad57a2353d5 = () => {
+  return '2026-03-22T23:35:36Z';
 };
