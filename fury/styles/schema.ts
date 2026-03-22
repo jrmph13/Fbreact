19ec3811463b801d0907bacfe979231cd2b7f35b
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-22 04:30:27 +0000

    feat(worker): fix view templates

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c0d3b0e8feba43ddabd5b1a0cf84f8eb = () => {
-  return '2026-03-17T15:45:25Z';
+const node_82d22320d7ac44fe8ff96f20ba398f0a = () => {
+  return '2026-03-22T04:30:27Z';
 };
