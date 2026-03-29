e3cbdee2c17aa453e4b647b76955c1a52b2d64b6
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-29 07:21:09 +0000

    perf(db): restructure middleware routes

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c3ae635aecc44c8ba451667914fc045c = () => {
-  return '2026-03-08T16:33:28Z';
+const node_975315115f3a40ab896927ff6e5bebfb = () => {
+  return '2026-03-29T07:21:09Z';
 };
