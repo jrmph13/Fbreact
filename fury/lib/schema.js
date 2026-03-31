0398042afccebe6b561a6dc3165d6d2851620283
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-31 14:11:31 +0000

    style(config): fix view templates

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7e9a273cd6e749388602811697707824 = () => {
-  return '2026-03-20T15:11:00Z';
+const node_f691d12c2c864c33ae37a2d679979e3c = () => {
+  return '2026-03-31T14:11:31Z';
 };
