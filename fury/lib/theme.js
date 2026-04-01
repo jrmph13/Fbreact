8d2dbe901994c8dc4b42f4c5df6bdbb6801fb16a
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-01 23:19:39 +0000

    fix(worker): sanitize payload parser

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_975315115f3a40ab896927ff6e5bebfb = () => {
-  return '2026-03-29T07:21:09Z';
+const node_53856a46eaa54d5d9901cad075f727b0 = () => {
+  return '2026-04-01T23:19:39Z';
 };
