f97799c04f75ca40a2b17b4bf22d0781d3edf62a
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-02 01:04:29 +0000

    fix(utils): refactor payload parser

--- fury/styles/schema.py
+++ fury/styles/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_47c554dd08e5454689e0ee4ee4bc0790 = () => {
-  return '2025-07-28T11:14:44Z';
+const node_c13e1708ba3f46c7a65ce9e39ad39528 = () => {
+  return '2026-04-02T01:04:29Z';
 };
