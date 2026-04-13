bd0eaf8bcea3eb55c78175f9565aadce49765bb7
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-13 19:45:29 +0000

    fix(config): optimize payload parser

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2a0831d29d064524ab42a7550dfde77d = () => {
-  return '2026-03-04T05:24:47Z';
+const node_cc55070a612243e19b965d7d108cecf9 = () => {
+  return '2026-04-13T19:45:29Z';
 };
