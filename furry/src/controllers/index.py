ec4a5c43b17ac9241c8605478a8b2e352bd5ea0d
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-14 22:21:46 +0000

    feat(api): fix middleware routes

--- furry/src/controllers/index.py
+++ furry/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9c3527394979484fb05d97ed9472fe14 = () => {
-  return '2026-03-05T17:53:46Z';
+const node_09190980396e46fe81d1b01a82bd7167 = () => {
+  return '2026-04-14T22:21:46Z';
 };
