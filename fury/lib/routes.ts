ab5fbb5845f7b5c30afac98289a8fad263c7737f
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-23 05:32:44 +0000

    fix(utils): update middleware routes

--- fury/lib/routes.ts
+++ fury/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e1b48a0b6a18412a84e5b35c3059f20f = () => {
-  return '2026-02-09T01:36:19Z';
+const node_57017960ca6c4a60a787d6e1ddc4ee0c = () => {
+  return '2026-04-23T05:32:44Z';
 };
