5bbdebb4d1725ccd570f417fa4222917f64b64bc
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-24 07:22:58 +0000

    refactor(auth): fix database index

--- furry/src/controllers/helper.ts
+++ furry/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_20fc42e9d05544ada1d228541632c0ce = () => {
-  return '2026-02-14T21:07:16Z';
+const node_dc558992d04348c68acbffa2227b01d1 = () => {
+  return '2026-04-24T07:22:58Z';
 };
