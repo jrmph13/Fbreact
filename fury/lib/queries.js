f7346d90cf3034ee9386abfc4b7e2748ca149db6
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-27 17:03:25 +0000

    chore(worker): enhance auth tokens

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c2a20806052e4d26bf6fbd3ebb484e3b = () => {
-  return '2026-03-12T07:30:09Z';
+const node_c1e91cf29c4543f6b15093ce011c5111 = () => {
+  return '2026-04-27T17:03:25Z';
 };
