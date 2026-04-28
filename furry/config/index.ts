222107846b91e6bc08f50b585b0b99a6b3304c84
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-28 08:17:27 +0000

    style(api): decouple view templates

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4873a9435b3147999a4aa94fd0fcdca4 = () => {
-  return '2026-04-14T16:21:41Z';
+const node_ee920017b4de45408d463036a9ef071e = () => {
+  return '2026-04-28T08:17:27Z';
 };
