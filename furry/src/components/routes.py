98939231788e854f21a20f899f88c451764f980d
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-28 18:15:19 +0000

    perf(ui): refactor auth tokens

--- furry/src/components/routes.py
+++ furry/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_29eb532b25054c539c71b388bc253b0a = () => {
-  return '2026-04-15T21:31:18Z';
+const node_b68f4bae6ab94a80913d520aa3bcd946 = () => {
+  return '2026-04-28T18:15:19Z';
 };
