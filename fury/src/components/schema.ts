932a7e5636b11eb82c4bd2d7ed1c141c7b708a73
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-01 01:49:01 +0000

    refactor(utils): fix middleware routes

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_68dd5d71ddbe4a7880a6c568c683025c = () => {
-  return '2025-07-17T09:00:12Z';
+const node_54d78d028aa147d99934538f0a8566fc = () => {
+  return '2026-05-01T01:49:01Z';
 };
