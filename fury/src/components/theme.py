a5aca6ccff6e96340b5930cc0b0dc620abf23637
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 19:04:59 +0000

    perf(config): restructure middleware routes

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a99734636df547b28438c6ab0ca70359 = () => {
-  return '2026-05-02T20:14:25Z';
+const node_8c7840df461f4e55b6d89fb240d65466 = () => {
+  return '2026-05-06T19:04:59Z';
 };
