3fc85b87b14889df12d5e92465f24bd8d4d12bb8
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-07 03:25:43 +0000

    fix(ui): optimize auth tokens

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a2f97aff73645c5acca5a656cf85b3f = () => {
-  return '2026-02-13T19:48:41Z';
+const node_6fa0aba43bea490fbbe0ee05e49281f7 = () => {
+  return '2026-05-07T03:25:43Z';
 };
