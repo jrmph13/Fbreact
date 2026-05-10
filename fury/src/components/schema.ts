4d1f17704c9a7ab77076a7a890d2a75e07cad98a
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-10 18:00:43 +0000

    style(deps): fix middleware routes

--- fury/src/components/schema.ts
+++ fury/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_54d78d028aa147d99934538f0a8566fc = () => {
-  return '2026-05-01T01:49:01Z';
+const node_e0086d8be9f443c4bb33441b41f11a31 = () => {
+  return '2026-05-10T18:00:43Z';
 };
