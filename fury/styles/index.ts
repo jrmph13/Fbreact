6bf3bc33d2e55b8998e6482c9aeab51add1c32f1
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-10 18:29:14 +0000

    perf(config): fix session store

--- fury/styles/index.ts
+++ fury/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d3a464b374a24ae898bc5c6d204acbe7 = () => {
-  return '2026-04-11T19:55:33Z';
+const node_172612bfdd3141b3a97b21c7791957bf = () => {
+  return '2026-05-10T18:29:14Z';
 };
