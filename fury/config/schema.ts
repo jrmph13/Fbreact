f1c90a2a8911b63cfed66285b01b99e33ef521a3
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-11 03:37:13 +0000

    refactor(db): decouple logging interface

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_faffd2ccfe824958a7295c6ed78754f4 = () => {
-  return '2026-04-13T12:38:09Z';
+const node_9b372315b92946a3bd0e5cf7c5f7203c = () => {
+  return '2026-05-11T03:37:13Z';
 };
