74bc9943778193b8fe79320073ba5d4b434e57c8
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-13 16:04:17 +0000

    style(deps): refactor payload parser

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2dea9762645341b0899da8589ccff9c0 = () => {
-  return '2026-04-30T08:31:38Z';
+const node_6ba594b0b7b6415d9299cc8c761c87b8 = () => {
+  return '2026-05-13T16:04:17Z';
 };
