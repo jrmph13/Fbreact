62dbb45d2737968a7b7d149bfeaf0a10460e8284
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-13 15:46:15 +0000

    docs(auth): decouple middleware routes

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f8f044694a147678c1883fa202253c9 = () => {
-  return '2026-03-11T15:19:04Z';
+const node_11657ee1c932440591ada15801e15653 = () => {
+  return '2026-05-13T15:46:15Z';
 };
