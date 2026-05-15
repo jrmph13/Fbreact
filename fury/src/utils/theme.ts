7c69208316ed2983111106e2a1062d116d098ffa
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-15 14:10:14 +0000

    perf(worker): optimize middleware routes

--- fury/src/utils/theme.ts
+++ fury/src/utils/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba5add51041d4bda9a7b6cf3da4286a9 = () => {
-  return '2026-02-27T13:04:09Z';
+const node_16c3e5c4017a414d8c9bac46e710b8ad = () => {
+  return '2026-05-15T14:10:14Z';
 };
