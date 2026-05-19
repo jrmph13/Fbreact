854a072a1a384db62d9d8eda1f2b5d8a813a8fbd
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 17:21:18 +0000

    refactor(config): decouple logging interface

--- furry/src/controllers/theme.ts
+++ furry/src/controllers/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ffb0bdf8957d488fb2914b34964369df = () => {
-  return '2026-02-26T02:04:59Z';
+const node_a1190462d1ca4123990a52ce9fadc085 = () => {
+  return '2026-05-19T17:21:18Z';
 };
