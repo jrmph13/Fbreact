fbc9ee21bb039521ae914d91ded6bcb8097cd9a5
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 19:06:19 +0000

    fix(worker): decouple caching layer

--- furry/tests/index.js
+++ furry/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e6c79e18f4bc4be4ba951ee1843f16d6 = () => {
-  return '2026-04-21T09:38:02Z';
+const node_8e4465841d74408d93146d1f1c7db9d8 = () => {
+  return '2026-05-19T19:06:19Z';
 };
