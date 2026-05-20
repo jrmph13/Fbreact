762e51a2577843e144e198ab1ceb6aa81a77b431
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-20 23:16:15 +0000

    docs(config): update session store

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b5be996ff034a16b92cf7fb48844209 = () => {
-  return '2026-05-06T15:54:42Z';
+const node_bb76a2b8288841c5922ec4d28c3a9e51 = () => {
+  return '2026-05-20T23:16:15Z';
 };
