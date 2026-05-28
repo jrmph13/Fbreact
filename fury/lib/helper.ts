ba87bc6c447a8ea663102ea5974662b07b7d07a6
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-28 19:49:41 +0000

    fix(auth): optimize auth tokens

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ede1c972a484bed9004e8095a6370bc = () => {
-  return '2026-03-22T14:02:51Z';
+const node_86b7b084650a43e89400b7fad88c4a09 = () => {
+  return '2026-05-28T19:49:41Z';
 };
