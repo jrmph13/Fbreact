967eb1a577e74571c3bd28a5e9d2b1e211bbe2eb
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-29 15:39:47 +0000

    style(core): optimize view templates

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_04c7c134e2014637a9e1ee0412b9ad3b = () => {
-  return '2026-05-15T15:22:03Z';
+const node_ede1633723234b48b1940c24e2853bb7 = () => {
+  return '2026-05-29T15:39:47Z';
 };
