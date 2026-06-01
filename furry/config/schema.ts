865ccaeff8660514a49186020849c8a3f9b8a84d
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-01 09:23:03 +0000

    docs(worker): optimize payload parser

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f32accd123fb46209a4e88fc735f3621 = () => {
-  return '2026-04-30T03:07:05Z';
+const node_ce8ce054528949ffb31248387ce9eb1c = () => {
+  return '2026-06-01T09:23:03Z';
 };
