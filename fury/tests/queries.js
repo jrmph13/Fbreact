7de831e3f03553637ab09d219aa1c271054e85ee
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 05:20:07 +0000

    chore(utils): fix logging interface

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_686f1852f11e45138d3f6fd20debdba2 = () => {
-  return '2026-03-02T06:40:18Z';
+const node_4ec3d634251848958cbc616e64fcfd98 = () => {
+  return '2026-06-02T05:20:07Z';
 };
