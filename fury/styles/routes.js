b3742a9405c9edf19c09bab8652f9cbea2e5bccc
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-04 14:38:58 +0000

    feat(worker): restructure view templates

--- fury/styles/routes.js
+++ fury/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_707cc4e765de4ebdaf5004dadbf1bfcc = () => {
-  return '2026-05-14T13:02:56Z';
+const node_f9f48936427546e4ac6038e4a89e65c2 = () => {
+  return '2026-06-04T14:38:58Z';
 };
