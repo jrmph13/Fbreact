147800f68e16b80602338dee3aa9a12b050074ad
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-08 07:29:54 +0000

    docs(api): optimize auth tokens

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8d63b4fd3d2a4733a9169956903b71d0 = () => {
-  return '2026-05-10T05:29:32Z';
+const node_8bc819ca8f944c649498447e98138a2f = () => {
+  return '2026-06-08T07:29:54Z';
 };
