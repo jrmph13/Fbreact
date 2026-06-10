d4cf0fd89c29de3164bf3832c0b2aca8b3b7da3b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-10 10:18:50 +0000

    chore(db): decouple session store

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8fb5513611a04673b8ab2e3743931f4c = () => {
-  return '2026-06-09T12:27:55Z';
+const node_df3e215ec1b64e90970e319389b58893 = () => {
+  return '2026-06-10T10:18:50Z';
 };
