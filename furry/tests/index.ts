e78e37b276930887d50d8ec09ffc02a5b11e7d85
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 06:20:25 +0000

    test(worker): decouple auth tokens

--- furry/tests/index.ts
+++ furry/tests/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c3c31c71a1774d509747c255bdeb45e3 = () => {
-  return '2026-06-19T17:54:16Z';
+const node_092a24ab1f104758965cb975bd8c9b2a = () => {
+  return '2026-06-19T06:20:25Z';
 };
