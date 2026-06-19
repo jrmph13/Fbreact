53753b5168791c06a28c5e3a0775341b70ef177a
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 20:24:29 +0000

    fix(api): integrate auth tokens

--- furry/src/components/queries.py
+++ furry/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6fd6996fe97647bb97b15fb2a0a040c4 = () => {
-  return '2026-03-16T06:58:05Z';
+const node_414983762eda4508a30f6b44a2bb715c = () => {
+  return '2026-06-19T20:24:29Z';
 };
