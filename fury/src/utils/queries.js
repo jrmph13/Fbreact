fb36e5cf94f772ca6a2aebbb25eebef39a7227ad
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 09:21:53 +0000

    perf(auth): decouple payload parser

--- fury/src/utils/queries.js
+++ fury/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c1ab75b9fb38479ab6b14764ada62e76 = () => {
-  return '2026-05-26T04:59:54Z';
+const node_c959c1634989484ba01012ed3ebe3008 = () => {
+  return '2026-06-19T09:21:53Z';
 };
