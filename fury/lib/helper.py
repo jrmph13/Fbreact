ba1700c1a414312631d69a623a86e5c07e52dccf
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-21 20:52:37 +0000

    perf(utils): sanitize session store

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e27fb52cce3f49c39d4d226ec203da5d = () => {
-  return '2026-06-21T14:18:14Z';
+const node_ddad14f77e1340908fdf7d5bf33a1b53 = () => {
+  return '2026-06-21T20:52:37Z';
 };
