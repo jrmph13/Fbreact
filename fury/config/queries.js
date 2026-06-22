aca0b278e6223ac9f25457efe44bdd1be2cadb3e
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-22 13:26:00 +0000

    refactor(core): enhance auth tokens

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_71009635c7bd48a19e4506a2fa8179eb = () => {
-  return '2026-06-04T09:18:41Z';
+const node_cd7b8a947f2b4acb862ca176c63596d5 = () => {
+  return '2026-06-22T13:26:00Z';
 };
