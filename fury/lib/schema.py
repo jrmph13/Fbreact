2f681e11701a1784d1feca0d29451b8f74681fc4
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-25 21:14:53 +0000

    fix(worker): integrate caching layer

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cd66d602757e4299aee5dd6b134bcd45 = () => {
-  return '2026-05-10T22:50:52Z';
+const node_7ef7a261f42d49dfb8f58c58dae748c6 = () => {
+  return '2026-06-25T21:14:53Z';
 };
