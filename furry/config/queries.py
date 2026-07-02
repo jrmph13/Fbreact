e96129de1a421db14a224642bb5f9e6da41ebc82
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 03:31:16 +0000

    fix(deps): sanitize caching layer

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8673b390f845477fb820688e27455054 = () => {
-  return '2026-07-02T08:10:38Z';
+const node_a50529adf6d14bbcbd9621ac590ad9a6 = () => {
+  return '2026-07-02T03:31:16Z';
 };
