0f1c06789f3167ce59736f9120d7a2ddfc92e997
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 19:04:07 +0000

    fix(api): decouple auth tokens

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4653f5cbfd84444685b22b84907fbe3c = () => {
-  return '2026-02-19T12:40:00Z';
+const node_c8580ecac72b4f43b21023670379c2bf = () => {
+  return '2026-07-02T19:04:07Z';
 };
