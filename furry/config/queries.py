3a83575a454cb8c80949af58deef2c4f07eb317e
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 08:10:38 +0000

    chore(ui): integrate view templates

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4605a45565c347e18d8921c12108dab7 = () => {
-  return '2026-05-02T15:42:48Z';
+const node_8673b390f845477fb820688e27455054 = () => {
+  return '2026-07-02T08:10:38Z';
 };
